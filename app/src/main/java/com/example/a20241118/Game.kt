6f6331e39ffe39943b6b39985d3ec9d4e110b188
student <juiklop3579@gmail.com>
2024-11-25 16:59:08 +0800

    v1

--- app/src/main/java/com/example/a20241118/Game.kt
+++ app/src/main/java/com/example/a20241118/Game.kt
@@ -1,16 +1,27 @@
 package com.example.a20241118
 
+import android.content.Context
+import android.media.MediaPlayer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
-class Game (val scope:CoroutineScope, val screenW:Int, val screenH:Int) {
+class Game (val scope:CoroutineScope, val screenW:Int, val screenH:Int,scale:Float,context: Context) {
 
     var counter = 0
     val state = MutableStateFlow(0)
     val background = Background(screenW)
     var isPlaying = true
+    val boy =Boy(screenH,scale)
+    val virus =Virus(screenW,screenH,scale)
+    val virus2 =Virus(screenW,screenH,scale)
+
+    //背景音樂
+    var mper1 = MediaPlayer.create(context, R.raw.lastletter)
+    //結束音樂
+    var mper2 = MediaPlayer.create(context, R.raw.gameover)
+
 
     fun Play() {
 
@@ -18,11 +29,31 @@ class Game (val scope:CoroutineScope, val screenW:Int, val screenH:Int) {
 
             //counter = 0
             isPlaying = true
+            virus.y=0
 
             while (isPlaying) {
+                mper1.start()  //播放背景音樂
 
-                delay(4)
+                delay(40)
                 background.Roll()
+
+                if(counter %3 == 0){
+                    boy.Walk()
+                    virus.Fly()
+                    virus2.Fly()
+
+                    //判斷是否碰撞，結束遊戲
+                    if(boy.getRect().intersect(virus.getRect())) {
+                        isPlaying = false
+                        //遊戲結束音效
+                        mper1.pause()
+                        mper2.start()
+
+                    }
+
+                }
+
+
                 counter++
                 state.emit(counter)
 
@@ -30,4 +61,11 @@ class Game (val scope:CoroutineScope, val screenW:Int, val screenH:Int) {
 
         }
     }
+    fun Restart(){
+        virus.Reset()
+        counter = 0
+        isPlaying = true
+        Play()
+    }
+
 }
